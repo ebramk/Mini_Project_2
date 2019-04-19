4ae2df703460cb1e4fc1f9dfdd29993f4f10ee0f
ebramk <er86@njit.edu>
2019-04-19 09:44:03 -0400

    Created a controller, route, and view to show the about page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,56 @@
 @extends('layouts.app')
 @section('content')
-    i am the about page
+   <h1> This project was created by Ebram Kerolos for IS 601 hosted by NJIT. Here are the project requirements: </h1>
+
+    <p>
+
+    1.  Make a new Laravel site.  <br />
+
+    2.  Generate the authentication. <br />
+
+    3.  Create a database seed that inserts 50 users  <br />
+
+    4.  Create a new model called car and add 3 properties to it: Make, Model, and year  <br />
+
+    5.  Create a factory for cars that sets the make field to either ford / honda / toyota  <br />
+
+    6.  Create a database seed to generate 50 cars.  <br />
+
+    7.  Create a controller, route, and view to show a contact form and add it to the main menu of your site.  <br />
+
+    8.  Create a controller, route, and view to show an about page and add it to the main menu of your site.  <br />
+
+    Unit tests - Each unit test uses an Assertion to check if it was done.  <br />
+
+    1.  Write a feature test to check if the register return a 200 status code.  <br />
+
+    2.  Write a feature test to check if the login pages return a 200 status code.  <br />
+
+    3.  Write a feature test to check if your about page returns a 200 status code.  <br />
+
+    4.  Write a feature test to check if your contact page returns a 200 status code.  <br />
+
+    5.  Create a unit test to insert a user into the users table.  <br />
+
+    6.  Create a unit test to update the name of a user in the database to Steve Smith.  <br />
+
+    7.  Create a unit test to delete a user in the database.  <br />
+
+    8.  Create a unit test to count the number of records inserted by the database seed.  i.e. $userCount = 50  <br />
+
+    9.  Create a unit test to test inserting a car.  <br />
+
+    10.  Create a unit test to test updating a car year to 2000.  <br />
+
+    11. Create a unit test to test deleting a car.  <br />
+
+    12.  Create a unit test to test to count the number of records inserted by the database seed.  i.e. $carCount = 50  <br />
+
+    13.   Create a unit test to check if a car's year is an integer.   <br />
+
+    14.  Create a unit test to check if a car's make is either ford / honda / toyota.   <br />
+
+    15.  Create a unit tests to check that the model is a string.   <br />
+
+     </p>
 @stop
