c31a2fa0e06fca8c7cd7b8182e60636d563c9cdf
ebramk <er86@njit.edu>
2019-04-20 12:54:39 -0400

    added text to home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    i am the home page
+   <h1> This project was created by Ebram Kerolos for IS 601 hosted by NJIT. I built this project using Laravel, Heroku, and Postgres database and PHP Storm.  </h1>
 @stop
